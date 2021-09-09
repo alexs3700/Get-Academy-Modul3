df7298b4850edf8305d19b6562eff3433b061c57
alexs3700 <alexs3700@gmail.com>
2021-09-09 13:52:47 +0200

    Add files via upload

---
+++ ChessGame_v1/Rook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleChess
+
+{
+    public class Rook : Piece
+    {
+        public Rook() : base("TRN")
+        {
+            string type = "Rook";
+            string symbol = "TRN";
+            
+            
+        }
+        public override bool Move(string fromPosition, string toPosition)
+        {
+            return fromPosition[0] == toPosition[0] || fromPosition[1] == toPosition[1];
+          
+            
+        }
+    }
+}
